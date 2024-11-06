773bd95f5884b14b51231b19f74cb7bccfd8360e
Leo <leonardo_zambrano@outlook.com>
2024-11-05 19:37:09 -0500

    🎉: Initial commit

---
+++ src/main/java/com/example/ArtifactBase/ArtifactBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArtifactBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactBaseApplication.class, args);
+	}
+
+}
